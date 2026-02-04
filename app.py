c6a6796f99c3b9a250fc55466bcf77b38bd29414
Zayed Ansari <ansarizayed04@gmail.com>
2026-02-04 16:24:21 +0530

    Improved translation quality with deep_translator

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import streamlit as st
-import whisper
 import tempfile
 import os
-st.title("Voice Message Translator")
+import whisper
+from deep_translator import GoogleTranslator
 
+
+st.title("Voice Message Translator")
 
 audio_file = st.file_uploader("Upload audio", type=["mp3", "wav", "m4a"])
 
-
-# Loading the model- using 'large' model for better results
 @st.cache_resource
 def load_model():
-    return whisper.load_model("large")  
-
+    return whisper.load_model('large')
 if audio_file:
     st.audio(audio_file)
 
@@ -20,35 +19,30 @@ if audio_file:
         with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
             tmp.write(audio_file.read())
             audio_path = tmp.name
-
         model = load_model()
-
-        with st.spinner("Processing..."):
-            # Hindi transcription
+        with st.spinner("Transcribing..."):
+      
+            #  Hindi transcription
             hindi_result = model.transcribe(
                 audio_path,
                 task="transcribe",
                 language="hi"
             )
 
-            # English translation
-            english_result = model.transcribe(
-                audio_path,
-                task="translate"
-            )
-
         os.remove(audio_path)
 
-        hindi_text = hindi_result["text"].strip()
-        english_text = english_result["text"].strip()
+        with st.spinner("Translating..."):
+                english_text = GoogleTranslator(
+                    source="hi",
+                    target="en"
+                ).translate(hindi_result['text'])
 
         st.success("Done!")
 
-     
-        st.markdown(
-            f"""
-**Original (Hindi):** {hindi_text}  
+        st.markdown(f"""
+**Original (Hindi):**  
+{hindi_result['text']}
 
-**English:** {english_text}
-"""
-        )
+**English:**  
+{english_text}
+""")
