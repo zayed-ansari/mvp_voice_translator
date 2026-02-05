1ea9452402d120bd5d87d09d02c20e0ad15839b6
Zayed Ansari <ansarizayed04@gmail.com>
2026-02-05 10:57:04 +0530

    Added voice recording feature

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Voice Message Translator
 
 MVP for translating voice messages between languages.
+The project idea is to basical
 
 ## Current Status (Day 1)
 - Whisper STT working
 - Streamlit UI for audio upload
 - Basic transcription functional
-- Translation component (next)
+- Translation component added with help of `deep_tranlator`
+- Speech to text then translate that text to another language (English)
 - TTS component (after that)
-
 ## Tech Stack
 - Whisper (local) for Speech-to-Text
+- Deep Translator for translation purspose  
 - Streamlit for UI
 - Python
+
 
 ## Usage
 ```bash
--- app.py
+++ app.py
@@ -3,26 +3,33 @@ import tempfile
 import os
 import whisper
 from deep_translator import GoogleTranslator
-
-
+from audiorecorder import audiorecorder
 st.title("Voice Message Translator")
 
 audio_file = st.file_uploader("Upload audio", type=["mp3", "wav", "m4a"])
-
+st.subheader("Record Audio")
+audio_rec = audiorecorder('Start Recording', 'Stop Recording')
 @st.cache_resource
 def load_model():
     return whisper.load_model('large')
+audio_path = None
 if audio_file:
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as tmp:
+         tmp.write(audio_file.read())
+         audio_path = tmp.name
     st.audio(audio_file)
+elif len(audio_rec) > 0:
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as tmp:
+         audio_rec.export(tmp.name, format='wav')
+         audio_path = tmp.name
+    st.audio(audio_path)
 
-    if st.button("Translate"):
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
-            tmp.write(audio_file.read())
-            audio_path = tmp.name
+try:
+    if audio_path and st.button('Translate'):
         model = load_model()
         with st.spinner("Transcribing..."):
-      
-            #  Hindi transcription
+        
+                #  Hindi transcription
             hindi_result = model.transcribe(
                 audio_path,
                 task="transcribe",
@@ -30,7 +37,7 @@ if audio_file:
             )
 
         os.remove(audio_path)
-
+        # Translation part (translating to english!)
         with st.spinner("Translating..."):
                 english_text = GoogleTranslator(
                     source="hi",
@@ -40,9 +47,12 @@ if audio_file:
         st.success("Done!")
 
         st.markdown(f"""
-**Original (Hindi):**  
-{hindi_result['text']}
+    **Original (Hindi):**  
+    {hindi_result['text']}
 
-**English:**  
-{english_text}
-""")
+    **English:**  
+    {english_text}
+    """)
+
+except Exception as e:
+     st.error(f"Failed to do the translation! {str(e)}")
